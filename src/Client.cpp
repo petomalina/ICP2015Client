d617d139598dbfc1c460d316923a3acf586dcef1
Gelidus <gelidus@hotmail.sk>
2015-03-14 17:06:42 +0100

    [Structure] Added structure elements

---
+++ src/Client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gelidus on 14.3.2015.
+//
+
+#include "Client.h"
---
+++ src/Client.h
@@ -0,0 +1,14 @@
+//
+// Created by gelidus on 14.3.2015.
+//
+
+#ifndef _ICP2015CLIENT_CLIENT_H_
+#define _ICP2015CLIENT_CLIENT_H_
+
+
+class Client {
+
+};
+
+
+#endif //_ICP2015CLIENT_CLIENT_H_
