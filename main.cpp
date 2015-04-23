a43915427918ef1b2a876cd787e7396a274b69dc
Gelidus <gelidus@hotmail.sk>
2015-04-23 14:29:45 +0200

    [Qt] Corrected libs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "src/Client.h"
 
+#include <QtCore/QtCore>
+
 int main (int argc, char *argv[])
 {
+	QCoreApplication application{argc, argv};
 	Client client{};
 
 	return static_cast<int>(client.run());
