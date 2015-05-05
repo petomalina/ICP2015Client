d0dba914658638a4e283fa4b154a491c07d4cef9
Gelidus <gelidus@hotmail.sk>
2015-05-05 12:19:13 +0200

    [GUIItem] Added base class for lab item

---
+++ src/views/gui/LabyrinthItem.h
@@ -0,0 +1,14 @@
+//
+// Created by gelidus on 5.5.2015.
+//
+
+#ifndef ICP2015CLIENT_LABYRINTHITEM_H
+#define ICP2015CLIENT_LABYRINTHITEM_H
+
+
+class LabyrinthItem {
+
+};
+
+
+#endif //ICP2015CLIENT_LABYRINTHITEM_H
