d7b73625f9b816d24220dca5b0a47d043f7a9750
Gelidus <gelidus@hotmail.sk>
2015-05-07 18:23:04 +0200

    [GUI] map generation scatch

--- src/views/GUIView.cpp
+++ src/views/GUIView.cpp
@@ -101,6 +101,34 @@ void GUIView::showMenu()
 
 void GUIView::generateMap()
 {
+	for (int x = 0; x < size; x++) {
+		for (int y = 0; y < size; y++) {
+
+			if (x == 0 && y == 0) {
+
+			} else if (x == 0 && y == size-1) {
+
+			} else if (x == size -1 && y == 0) {
+
+			} else if (x == size -1 && y == size -1) {
+
+			} else if (x % 2 == 0 && y % 2 == 0) {
+				// calculate T-s
+				if (x == 0) { // up
+
+				} else if (y == 0) { // left
+
+				} else if (x == size -1) { // down
+
+				} else if (y == size -1) { // right
+
+				}
+
+			} else if (x % 2 != 0 && y % 2 != 0) {
+				// randomize
+			}
+		}
+	}
 }
 
 
