657f79e0f963c2779d55b28adeee96dfc7c648b4
Gelidus <gelidus@hotmail.sk>
2015-05-08 17:56:21 +0200

    [Event] Added

---
+++ src/event/Event.h
@@ -0,0 +1,76 @@
+#ifndef __Event__H
+#define __Event__H
+
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <memory>
+
+#include <assert.h>
+
+template <typename ...Arguments>
+class Event {
+
+public: // types
+  using EventFunction = std::shared_ptr<std::function<void(Arguments...)>>;
+
+private:
+  std::vector<std::shared_ptr<std::function<void(Arguments...)>>> listeners;
+
+public:
+  Event() { }
+
+  ~Event() {
+    for (auto callback : this->listeners) {
+      callback.reset();
+    }
+  }
+
+  virtual void dispatch(Arguments... params) {
+    for (auto callback : this->listeners) {
+      callback->operator()(params...);
+    }
+  }
+
+  // lambda function attach
+  EventFunction attach(std::function<void(Arguments...)> function) {
+    auto listener = EventFunction(new std::function<void(Arguments...)>(function));
+
+    return this->attach(listener);
+  }
+
+  // overload operator for attaching lambdas
+  Event& operator()(std::function<void(Arguments...)> function) {
+    this->attach(function);
+
+    return *this;
+  }
+
+  EventFunction attach(EventFunction listener) {
+    this->listeners.push_back(listener);
+
+    return listener;
+  }
+
+  // overload operator for attaching
+  Event& operator()(EventFunction listener) {
+    this->attach(listener);
+
+    return *this;
+  }
+
+  // overload operator for dispatch
+  Event& operator()(Arguments ...params) {
+    this->dispatch(params...);
+  }
+
+  void detach(EventFunction listener) {
+    auto iterator = std::find(this->listeners.begin(), this->listeners.end(), listener);
+
+    assert(iterator != this->listeners.end());
+
+    this->listeners.erase(iterator);
+  }
+};
+
+#endif
---
+++ src/event/EventListener.h
@@ -0,0 +1,62 @@
+/*
+  This class is able to handle multiple events.
+
+  @author: Gelidus
+*/
+
+#ifndef __EventListener__H
+#define __EventListener__H
+
+#include "Event.h"
+#include <map>
+
+template <typename MapType>
+class EventListener {
+
+private:
+	std::map<MapType, Event<>*> events;
+
+public:
+	EventListener() { }
+	virtual ~EventListener() {
+		for(auto event : this->events) {
+			delete event.second;
+		}
+	}
+
+	template <typename ...Arguments>
+	void on(MapType eventType, std::function<void(Arguments...)> function) {
+		if (this->events.find(eventType) == this->events.end()) { // event does not exist, create new
+			this->createEvent<Arguments...>(eventType);
+		}
+
+		reinterpret_cast<Event<Arguments...>*>(this->events[eventType])->attach(function);
+	}
+
+	template <typename ...Arguments>
+	void createEvent(MapType eventType) {
+		this->events.insert(std::pair<MapType, Event<>*>(eventType, reinterpret_cast<Event<>*>(new Event<Arguments...>())));
+	}
+
+	template <typename ...Arguments>
+	Event<Arguments...> *get(MapType eventType) {
+		auto iterator = this->events.find(eventType);
+		if (iterator == this->events.end()) {
+			this->createEvent<Arguments...>(eventType);
+			return this->get<Arguments...>(eventType);
+		} else {
+			return reinterpret_cast<Event<Arguments...>*>(iterator->second);
+		}
+	}
+
+	template <typename ...Arguments>
+	void dispatch(MapType eventType, Arguments... params) {
+
+		auto iterator = this->events.find(eventType);
+		if (iterator != this->events.end()) { // event found
+			this->get<Arguments...>(eventType)->dispatch(params...);
+		}
+	}
+};
+
+#endif
