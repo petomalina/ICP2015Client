4872026f60d32ab328a5b7ae6984c8b53895945a
Gelidus <gelidus@hotmail.sk>
2015-05-08 17:56:31 +0200

    [Event] Added events

--- src/views/IView.h
+++ src/views/IView.h
@@ -5,12 +5,35 @@
 #ifndef ICP2015CLIENT_IVIEW_H
 #define ICP2015CLIENT_IVIEW_H
 
+#include "../event/Event.h"
+
+enum class Rotation {
+	Up, Down, Left, Right
+};
+
+enum class FragmentType {
+	T, L, I
+};
 
 class IView {
 
 public:
 	virtual void initialize() = 0;
 	virtual void show() = 0;
+
+public:
+	// rotation of character
+	Event<Rotation> onCharacterMove;
+	// index and what type
+	Event<int, FragmentType, Rotation> onFragmentPlace;
+	// number of players, size of playground
+	Event<int, int> onGameStart;
+
+	Event<> onUndo;
+
+	Event<> onRedo;
+
+	Event<> onSave;
 };
 
 
