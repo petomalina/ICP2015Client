e7328b8d3f2a9c6893c09e5acc80db48eb3ac1c7
Vojtech Mašek <shooter.vm@gmail.com>
2015-05-12 11:38:05 +0200

    [Main] CLI switch added

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication application{argc, argv};
+	QApplication application{argc, argv}; //FIXME: will it be correct to use Qapp when cli is on ?
 
 	int playgroundSize = 7;
-	srand(time(NULL)); //seed for pseudo random number generating
+	srand((unsigned)time(NULL)); //seed for pseudo random number generating
 
+	//Preprocessor define macro must be set to run game in CLI mode, use -DCLI_MODE flag when compiling program
+#ifndef CLI_MODE
 	GUIView view{};
-	//CLIView view{};
+#else
+	CLIView view{};
+#endif
 
 	Game game{&view};
 	game.run();
